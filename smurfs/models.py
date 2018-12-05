e675018a982d1c5772ee20fbea86af13bed26d87
Tom Tarpey <tomtarpeydev@gmail.com>
2018-12-05 18:06:23 +0000

    (smurfs) : done day 1 mvp (up to added secret key to env)

---
+++ smurfs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from uuid import uuid4
+
+# Create your models here.
+
+class Smurf(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=300)
+    age = models.CharField(max_length=200)
+    height = models.CharField(max_length=80)
